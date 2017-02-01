9bfa572689ff59713408c932f5f4df6162f1a602
Wardle, Iain <iain.wardle@sage.com>
2017-02-01 18:25:57 +0000

    Initial Commit

--- TestCI/TestCI/App.cs
+++ TestCI/TestCI/App.cs
@@ -11,15 +11,22 @@ namespace TestCI
 	{
 		public App ()
 		{
-			// The root page of your application
-			MainPage = new ContentPage {
-				Content = new StackLayout {
-					VerticalOptions = LayoutOptions.Center,
-					Children = {
-						new Label {
-							HorizontalTextAlignment = TextAlignment.Center,
-							Text = "Welcome to Xamarin Forms!"
-						}
+            // The root page of your application
+            MainPage = new ContentPage {
+                Content = new StackLayout {
+                    VerticalOptions = LayoutOptions.Center,
+                    Children =
+                    {
+                        new Label
+                        {
+                            HorizontalTextAlignment = TextAlignment.Center,
+                            Text = "Welcome to Xamarin Forms2!"
+                        },
+                        new Button
+                        {
+                            Text = "Exit"                                                        
+                        }
+
 					}
 				}
 			};
